b313492d6224c4e12c8e5264e60a6d24f5ca3d5f
Baptiste <baptiste.colin@outlook.com>
2017-10-04 18:01:20 +0200

    problem 9

--- problems.hs
+++ problems.hs
@@ -53,3 +53,8 @@ compress l = case l of
 				[] -> []
 				[a] -> [a]
 				(hd1:(hd2:tl)) -> if hd1==hd2 then compress (hd1:tl) else hd1:(compress (hd2:tl)) 
+
+pack :: Eq a => [a] -> [[a]]
+
+pack [] = []
+pack (x:xs) = let (first,rest) = span (==x) xs in (x:first) : pack rest
