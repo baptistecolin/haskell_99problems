edbe42c3cc5b8edb2e46249c08f5e885507f0782
Baptiste <baptiste.colin@outlook.com>
2017-10-04 16:30:46 +0200

    problem 2

--- problems.hs
+++ problems.hs
@@ -6,3 +6,11 @@ myLast :: [a] -> Maybe a
 myLast [] = Nothing
 myLast [x] = Just x
 myLast (hd:tl) = myLast tl
+
+myButLast :: [a] -> Maybe a
+
+myButLast l = case l of
+				[] -> Nothing
+				[_] -> Nothing
+				[a,_] -> Just a
+				(hd:tl) -> myButLast tl
