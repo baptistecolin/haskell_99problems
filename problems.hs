46f6febef9a8d1b2ed406b948f51c1a62444eb9e
Baptiste <baptiste.colin@outlook.com>
2017-10-05 09:25:18 +0200

    problem 10

--- problems.hs
+++ problems.hs
@@ -58,3 +58,7 @@ pack :: Eq a => [a] -> [[a]]
 
 pack [] = []
 pack (x:xs) = let (first,rest) = span (==x) xs in (x:first) : pack rest
+
+encode :: Eq a => [a] -> [(Int, a)]
+
+encode xs = map (\x -> (length x, head x)) (group xs)
