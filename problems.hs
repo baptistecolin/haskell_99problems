c0f6b701af6d0551f1f61bed8e39ecdf3e47636c
Baptiste <baptiste.colin@outlook.com>
2017-10-05 10:46:01 +0200

    its better with comments

--- problems.hs
+++ problems.hs
@@ -1,12 +1,14 @@
 import Data.List
 import Data.Maybe
 
+-- problem 1
 myLast :: [a] -> Maybe a
 
 myLast [] = Nothing
 myLast [x] = Just x
 myLast (_:tl) = myLast tl
 
+-- problem 2
 myButLast :: [a] -> Maybe a
 
 myButLast l = case l of
@@ -15,18 +17,21 @@ myButLast l = case l of
 				[a,_] -> Just a
 				(_:tl) -> myButLast tl
 
+-- problem 3
 elementAt :: [a] -> Integer -> Maybe a
 
 elementAt (hd:_) 1 = Just hd
 elementAt [] i = Nothing
 elementAt (_:tl) i = elementAt tl (i-1)
 
+-- problem 4
 myLength :: [a] -> Integer
 
 myLength l = case l of
 				[] -> 0
 				(hd:tl) -> (1+ (myLength tl))
 
+-- problem 5
 myReverse :: [a] -> [a]
 
 myReverse l = case l of
@@ -34,12 +39,14 @@ myReverse l = case l of
 				[a] -> [a]
 				(hd:tl) -> ((myReverse tl)++[hd])
 
+-- problem 6
 isPalindrome :: Eq a => [a] -> Bool
 
 isPalindrome l = (l == myReverse l)
 
 data NestedList a = Elem a | List [NestedList a]
 
+-- problem 7
 flatten :: NestedList a -> [a]
 
 flatten l = case l of
@@ -47,6 +54,7 @@ flatten l = case l of
 				List [] -> []
 				List (hd:tl) -> (flatten hd)++(flatten (List tl))
 
+-- problem 8
 compress :: Eq a => [a] -> [a]
 
 compress l = case l of
@@ -54,11 +62,13 @@ compress l = case l of
 				[a] -> [a]
 				(hd1:(hd2:tl)) -> if hd1==hd2 then compress (hd1:tl) else hd1:(compress (hd2:tl)) 
 
+-- problem 9
 pack :: Eq a => [a] -> [[a]]
 
 pack [] = []
 pack (x:xs) = let (first,rest) = span (==x) xs in (x:first) : pack rest
 
+-- problem 10
 encode :: Eq a => [a] -> [(Int, a)]
 
 encode xs = map (\x -> (length x, head x)) (group xs)
@@ -66,6 +76,7 @@ encode xs = map (\x -> (length x, head x)) (group xs)
 data ListItem a = Single a | Multiple Int a
 	deriving(Show)
 
+-- problem 11
 encodeModified :: Eq a => [a] -> [ListItem a]
 
 encodeModified = map encodeHelper . encode
@@ -73,6 +84,7 @@ encodeModified = map encodeHelper . encode
 		encodeHelper (1,a) = Single a
 		encodeHelper (n,a) = Multiple n a
 
+-- problem 12
 decodeModified :: Eq a => [ListItem a] -> [a]
 
 decodeModified = concat . map decodeAux
@@ -80,3 +92,5 @@ decodeModified = concat . map decodeAux
 		decodeAux (Single x) = [x]
 		decodeAux (Multiple 2 x) = x:(decodeAux (Single x))
 		decodeAux (Multiple n x) = x:(decodeAux (Multiple (n-1) x))
+
+-- problem 13
