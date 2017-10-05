2205d8d61bbb97f12a12db96a6ca31e598cfb2e0
Baptiste <baptiste.colin@outlook.com>
2017-10-05 09:46:24 +0200

    problem 11

--- problems.hs
+++ problems.hs
@@ -62,3 +62,13 @@ pack (x:xs) = let (first,rest) = span (==x) xs in (x:first) : pack rest
 encode :: Eq a => [a] -> [(Int, a)]
 
 encode xs = map (\x -> (length x, head x)) (group xs)
+
+data ListItem a = Single a | Multiple Int a
+	deriving(Show)
+
+encodeModified :: Eq a => [a] -> [ListItem a]
+
+encodeModified = map encodeHelper . encode
+	where
+		encodeHelper (1,a) = Single a
+		encodeHelper (n,a) = Multiple n a
